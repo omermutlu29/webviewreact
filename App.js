5a8168b6c38c602cec6dc803cf1153a4d989cce5
omermutlu29@gmail.com <omermutlu29@gmail.com>
2020-12-11 16:22:04 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {WebView} from 'react-native-webview';
+const App: () => React$Node = () => {
+    return (git init
+      <WebView style={{width:'100%',height:'100%'}} source={{uri: 'https://ddiamondstone.com/login.php'}}/>
+    );
+};
+
+export default App;
